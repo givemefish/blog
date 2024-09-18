fa40cf4ac19f422bea8c17f252cc06da84a04242
陳佳駿 <chiachunchen@yuanta.com>
2024-09-18 15:17:38 +0800

    Init commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'JC Notes',
+  description: '',
+  themeConfig: {
+    logo: '/logo.jpg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首頁', link: '/' },
+      { text: '程式', link: '/programming' },
+      { text: '書法', link: '/calligraphy' }
+    ],
+    outline: {
+      label: '目錄',
+    },
+    docFooter: {
+      prev: '上一頁',
+      next: '下一頁'
+    },
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright ©陳佳駿",
+    },
+    search: {
+      provider: 'local',
+    },
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium',
+      },
+    },
+    sidebar: {
+      collapsed: true,
+      text: 'TGapi文档',
+      items: [
+        { text: 'TGapi登陆注册', link: '/item-a' },
+        { text: 'Item B', link: '/item-b' },
+      ]
+    },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/givemefish' }],
+    base: '/blog/',
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "JC Notes"
+  text: ""
+  tagline: Personal notes
+  actions:
+    - theme: brand
+      text: 隨筆      
+    - theme: alt
+      text: 程式
+      link: /programming
+    - theme: alt
+      text: 書法
+      link: /calligraphy
+
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
